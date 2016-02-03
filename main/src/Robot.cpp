921b935b74b6b2119402117052d3890ec5cbe4ba
Corey Bastarache <corey.bastarache@hotmail.com>
2016-02-02 23:06:28 -0500

    fixed set up for navx (mac)

--- main/src/Robot.cpp
+++ main/src/Robot.cpp
@@ -1,5 +1,6 @@
 #include "WPILib.h"
 #include <math.h>
+#include <AHRS.h>
 
 #define RES_X 640
 #define RES_Y 480
@@ -212,15 +213,7 @@ private:
 			DriverStation::ReportError("IMAQdxConfigureGrab error: " + std::to_string((long)imaqError) + "\n");
 		}
 	}
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
 	void AutonomousInit(void)
 	{
 
