f71738e22f570bca7590e723330a86556f115404
Corey Bastarache <corey.bastarache@hotmail.com>
2016-02-10 10:58:49 -0500

    finished merge. removed some files that didnt need tracking

--- main/linux_simulate/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lwpilibcSim -lgz_msgs -lntcore -lgazebo_client -lboost_system
-
