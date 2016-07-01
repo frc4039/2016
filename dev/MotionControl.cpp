1fe8574c754c54f8b27415241014d4a34f2c7ca2
Noah Linton <nlinton123@hotmail.com>
2016-07-01 19:10:21 -0400

    Added MotionControl files to repository

---
+++ dev/MotionControl.cpp
@@ -0,0 +1,23 @@
+//
+//  MotionControl.cpp
+//  
+//
+//  Created by Noah Linton on 2016-07-01.
+//
+//
+
+#include "MotionControl.hpp"
+
+
+class MotionControl
+{
+private:
+    //define motors somehow
+    
+public:
+    setVelocity() // set velocity
+    {
+        
+    }
+    
+};
---
+++ dev/MotionControl.hpp
@@ -0,0 +1,14 @@
+//
+//  MotionControl.hpp
+//  
+//
+//  Created by Noah Linton on 2016-07-01.
+//
+//
+
+#ifndef MotionControl_hpp
+#define MotionControl_hpp
+
+#include <stdio.h>
+
+#endif /* MotionControl_hpp */
