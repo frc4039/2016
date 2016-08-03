d230807f9a0129f3575c3716d0cb2d9fa3dd8885
Noah Linton <nlinton123@hotmail.com>
2016-08-03 18:03:39 -0400

    [Noah L] Updates to the MotionControl.cpp

--- dev/MotionControl.cpp
+++ dev/MotionControl.cpp
@@ -13,10 +13,28 @@ class MotionControl
 {
 private:
     //define motors somehow
+    VictorSP *leftDrive4;
+    VictorSP *leftDrive1;
+    VictorSP *rightDrive2;
+    VictorSP *rightDrive3;
+    
+    // Gear definitions
+    
+    
+    // gear ratio calculation
+    float gearRatio()
+    {
+        
+    }
+    
     
 public:
-    setVelocity() // set velocity
+    void setVelocity(velocity) // set velocity
     {
+        leftDrive4->SetSpeed(velocity);
+        leftDrive1->SetSpeed(velocity);
+        rightDrive2->SetSpeed(-velocity);
+        rightDrive3->SetSpeed(-velocity);
         
     }
     
