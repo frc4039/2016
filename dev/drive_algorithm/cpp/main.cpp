1285362dcc099699d3f5bc254c87e32427785558
Corey Bastarache <corey.bastarache@hotmail.com>
2016-11-03 16:22:11 -0400

    files

---
+++ dev/drive_algorithm/cpp/main.cpp
@@ -0,0 +1,39 @@
+#include "path.h"
+#include "pathcurve.h"
+#include "pathfollower.h"
+#include "pathline.h"
+
+int main(){
+	Path *auto1;
+	Path *curve;
+	PathFollower *robot;
+
+	int start[2] = {0,0};
+	int end[2] = {16000,0};
+	int c2[2] = {8000, 0};
+	int c3[2] = {16000, -5000};
+	int c4[2] = {0, -5000};
+
+	auto1 = new PathLine(start,end, 100);
+	//auto1->calculate();
+	
+	curve = new PathCurve(end,c2,c3,c4,200);
+	//curve->calculate();
+
+	robot = new PathFollower();
+
+	auto1->add(curve);
+	auto1->show();
+
+	float left, right;
+	int x = 0, y = 0;
+	robot->initPath(auto1, PathForward);
+
+	/*while (robot->followPath(nav->GetYaw(), &left, &right) == 0){
+		leftDrive->set(left)*/
+
+	robot->updatePos();
+	//}
+
+	return 0;
+}
---
+++ dev/drive_algorithm/cpp/pathfollower.h
@@ -0,0 +1,38 @@
+#ifndef __PATH_FOLLOWER__
+#define __PATH_FOLLOWER__
+
+#include "path.h"
+
+typedef enum PathFollower_directions_enum{
+	PathForward = 0,
+	PathBackward = 1,
+} PathDirection;
+
+class PathFollower
+{
+private:
+	int posX, posY;
+	int lastX, lastY;
+	int nextPoint;
+	float angle;
+	PathDirection direction;
+	float distanceToEnd;
+	float maxSpeed;
+	Path *path;
+	float leftSpeed, rightSpeed;
+	void driveToPoint(void);
+	float distanceP;
+	float turnP;
+	float turnSpeed;
+	float normalize(float normalAngle);
+
+public:
+	PathFollower();
+	void initPath(Path *nPath, PathDirection nDirection);
+	int followPath(int nPosX, int nPosY, float nAngle, float *nLeftSpeed, float *nRightSpeed);
+	void setSpeed(float nMaxSpeed, float nP);
+	void updatePos(void);
+
+};
+
+#endif
